a4afd10279d3872683643059e0367f9a89ccf336
Binocla <tdoktora@inbox.ru>
2024-01-09 23:35:03 +0400

    Some refactoring

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Main {
     public static void main(String[] args) {
         // Man - ascender
@@ -18,7 +20,23 @@ public class Main {
         Man[] men = {m1, m2, m3};
         for (int i = 0; i < men.length; i++) {
             men[i].method1();
-
         }
+
     }
+
+//    public boolean equals(Object o) {
+//        if (this == o) {
+//            return true;
+//        }
+//        if (o == null || o.getClass() != getClass()) {
+//            return false;
+//        }
+//
+//        Main m = (Main) o; // раз наш Object o такого же типа как и этот класс - давай его сузим до нашего этого (данного) класса!
+//
+//        if (m.getAge() != age) {
+//            return false;
+//        }
+//        // сначала сравниваем примитивные типы данных, а потом уже - ссылочные
+//    }
 }
